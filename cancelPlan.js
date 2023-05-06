2439d469fd9bffa3fc2f1c37de6884162c3dc3e5
Globetrottercode <dassouvik180@gmail.com>
2023-05-06 21:21:35 +0530

    Change in cancelPlan

--- cancelPlan.js
+++ cancelPlan.js
@@ -1,18 +1,24 @@
-let date = new Date();
-let eDay = "10/6/2023";
-let month = date.getMonth() + 1;
-let pDay = date.getDate() + "/" + month + "/" + date.getFullYear();
-let curr = pDay;
-let i = 0;
-while (curr !== eDay) {
-  date.setDate(date.getDate() + 1);
+function getCancelDates(eDay) {
+  let arr = [];
+  let date = new Date();
+  let month = date.getMonth() + 1;
+  let pDay = date.getDate() + "/" + month + "/" + date.getFullYear();
+  let curr = pDay;
+  let i = 0;
+  while (curr !== eDay) {
+    date.setDate(date.getDate() + 1);
 
-  curr =
-    date.getDate() +
-    "/" +
-    (date.getMonth() + 1).toString() +
-    "/" +
-    date.getFullYear();
-  console.log(curr);
-  i++;
+    curr =
+      date.getDate() +
+      "/" +
+      (date.getMonth() + 1).toString() +
+      "/" +
+      date.getFullYear();
+    // console.log(curr);
+    arr.push(curr);
+    i++;
+  }
+  return arr;
 }
+let eDay = "10/6/2023";
+console.log(getCancelDates(eDay));
